b9800916aad59309efa2325e913bfa14e6ca6ec2
poissonpie <poissonpie@gmail.com>
2012-11-22 13:12:50 +0100

    add Player class

--- elo.rb
+++ elo.rb
@@ -1,6 +1,12 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 
+class Player
+  attr_accessor :rating
+end
+
+
+
 describe "result" do
   describe "when both players start with rating of 100 and k factor of 30" do
     it "must return 115 for the winner" do
