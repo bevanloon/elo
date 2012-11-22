d4fbfee5b46125ad7932f56b48ec76aade61ef46
poissonpie <poissonpie@gmail.com>
2012-11-22 13:13:48 +0100

    add init to Player

--- elo.rb
+++ elo.rb
@@ -3,6 +3,10 @@ require 'minitest/autorun'
 
 class Player
   attr_accessor :rating
+
+  def initialize(rating=100)
+    @rating = rating
+  end
 end
 
 
@@ -10,6 +14,8 @@ end
 describe "result" do
   describe "when both players start with rating of 100 and k factor of 30" do
     it "must return 115 for the winner" do
+      player = Player.new
+      new_rating = player.rating
       new_rating.must_equal 115
     end
   end
