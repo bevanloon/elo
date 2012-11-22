cec00ef09b7e496b9c1747a427b4b24b11fbe34f
poissonpie <poissonpie@gmail.com>
2012-11-22 17:27:20 +0100

    calculate new rating

--- elo.rb
+++ elo.rb
@@ -11,8 +11,8 @@ class Player
   def expected_outcome(opponent_rating)
     1.0/(1 + 10**((opponent_rating - @rating)/400))
   end
-  def calculate_new_rating
-    @rating = 115
+  def calculate_new_rating(score, opponent_rating)
+    @rating = @rating + 30 * (score - expected_outcome(opponent_rating))
   end
 end
 
@@ -22,7 +22,7 @@ describe "result" do
   describe "when both players start with rating of 100 and k factor of 30" do
     it "must return 115 for the winner" do
       winner = Player.new
-      winner.calculate_new_rating
+      winner.calculate_new_rating(1, 100)
       new_rating = winner.rating
       new_rating.must_equal 115
     end
@@ -30,7 +30,7 @@ describe "result" do
   describe "expected outcome" do
     it "must return 0.5 when ratings are equal" do
       winner = Player.new
-      winner.expected_outcome.must_equal 0.5
+      winner.expected_outcome(100).must_equal 0.5
     end
   end
 
