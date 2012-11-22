bb71167f22dc07379c6c18ad7e17c0d776f75a26
poissonpie <poissonpie@gmail.com>
2012-11-22 13:05:57 +0100

    first test

---
+++ elo.rb
@@ -0,0 +1,11 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+
+describe "result" do
+  describe "when both players start with rating of 100 and k factor of 30" do
+    it "must return 115 for the winner" do
+      new_rating.must_equal 115
+    end
+  end
+end
+
